53dd395f2c62b3a922165a49116bb66d84044d51
Rahil Ansari <77235850+rahilmarch@users.noreply.github.com>
2021-12-01 16:07:57 +0530

    update

--- lib/Screens/Authentication/authentication.dart
+++ lib/Screens/Authentication/authentication.dart
@@ -36,7 +36,7 @@ class _AuthenticationState extends State<Authentication> {
         image: DecorationImage(
           image: AssetImage('assets/Images/background_image.jpg'),
           fit: BoxFit.fill,
-          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.8), BlendMode.dstATop),
+          colorFilter: ColorFilter.mode(Colors.black.withOpacity(0.6), BlendMode.dstATop),
         ),
       ),
       child: Scaffold(
