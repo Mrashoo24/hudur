0a7b7287212c6775ae2fa1a6e2ea98cd6a9ad62d
mrashoo24 <arsalank28@gmail.com>
2021-12-11 17:02:05 +0530

    BNB REMOVED

--- lib/Screens/Authentication/authentication.dart
+++ lib/Screens/Authentication/authentication.dart
@@ -62,7 +62,7 @@ class _AuthenticationState extends State<Authentication> {
                           Container(
                             margin: const EdgeInsets.only(top: 20),
                             padding: const EdgeInsets.all(8.0),
-                            child: Image.asset('assets/Images/logo.png'),
+                            child: Image.asset('assets/Images/big_logo.png'),
                           ),
                           Container(
                             padding: const EdgeInsets.all(40.0),
