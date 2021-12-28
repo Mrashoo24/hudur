cda66753bfe09cb9f13256f9671162b006eca949
Rahil Ansari <workbyrahil@gmail.com>
2021-12-28 21:07:33 +0530

    commit

--- lib/Screens/Services/services.dart
+++ lib/Screens/Services/services.dart
@@ -1,128 +1,193 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:hudur/Components/api.dart';
 import 'package:hudur/Components/colors.dart';
 import 'package:hudur/Components/models.dart';
+import 'package:intl/intl.dart';
 
 class Services extends StatefulWidget {
-  final ServiceModel serviceModel;
-  const Services({ Key key, this.serviceModel }) : super(key: key);
+  final UserModel userModel;
+  const Services({ Key key, this.userModel }) : super(key: key);
 
   @override
   _ServicesState createState() => _ServicesState();
 }
 
 class _ServicesState extends State<Services> {
-  String valueChoose;
-  List listItem = [
-    "Certificate with Detailed Salary",
-    "Certificte with Total Salary",
-    "Certificate with Without Salary",
-    "Others",
-  ]; 
-  final AllApi api = AllApi();
-  final TextEditingController refController = TextEditingController();
-  final TextEditingController certController = TextEditingController();
-  final TextEditingController dateController = TextEditingController();
+
+    int _selectedValue = 0;
+
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+
+  bool _trySubmit() {
+    var _isValid = _formkey.currentState.validate();
+    if (_isValid) {
+      FocusScope.of(context).unfocus();
+      _formkey.currentState.save();
+    }
+    return _isValid;
+  }
+
+  Widget _radioListTile({
+    String title,
+    String subtitle,
+    int value,
+    List details,
+  }) {
+    var textFieldValues = [];
+    return RadioListTile(
+      activeColor: hippieBlue,
+      secondary: ElevatedButton(
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(12.0)))),
+            backgroundColor: MaterialStateProperty.all<Color>(hippieBlue)),
+        child: const Text('Details'),
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (ctx) {
+              return AlertDialog(
+                title: const Text('Details'),
+                content: Form(
+                  key: _formkey,
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width,
+                    height: MediaQuery.of(context).size.height,
+                    child: ListView.builder(
+                      itemCount: details.length,
+                      itemBuilder: (ctx, index) {
+                        return TextFormField(
+                          decoration: InputDecoration(
+                            hintText: details[index],
+                          ),
+                          onSaved: (value) {
+                            textFieldValues.add(value);
+                          },
+                          validator: (value) {
+                            if (value.isEmpty) {
+                              return 'Please fill out this field';
+                            }
+                            return null;
+                          },
+                        );
+                      },
+                    ),
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                    onPressed: () async {
+                      var _canSubmit = _trySubmit();
+                      if (_canSubmit) {
+                        await AllApi().postLeaveRequest(
+                          widget.userModel.phoneNumber,
+                          DateFormat('dd-MM-yyyy').format(DateTime.now()),
+                          title,
+                          textFieldValues,
+                        );
+                        Get.back();
+                      }
+                    },
+                    child: const Text('Submit'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Get.back();
+                    },
+                    child: const Text('Cancel'),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+      value: value,
+      title: Text(
+        title,
+      ),
+      subtitle: Text(
+        subtitle,
+      ),
+      groupValue: _selectedValue,
+      onChanged: (value) => setState(
+        () {
+          _selectedValue = value;
+        },
+      ),
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      
-         child: Center(
-           child: Column(
-             children: [
-               Container(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Container(
-                          padding: EdgeInsets.only(left: 16, right: 16),
-                          decoration: BoxDecoration(
-                            border: Border.all(width: 1, color: Colors.black),
-                            borderRadius: BorderRadius.circular(15)
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          image: const AssetImage('assets/Images/background_image.jpg'),
+          fit: BoxFit.fill,
+          colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.6), BlendMode.dstATop),
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          title: const Text('Leaves'),
+          backgroundColor: hippieBlue,
+        ),
+        body: SingleChildScrollView(
+          child: FutureBuilder<List<LeaveRequestsModel>>(
+            future: AllApi().getLeaveRequests(),
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return Center(
+                  child: Image.asset('assets/Images/loading.gif'),
+                );
+              } else {
+                var leaveRequests = snapshot.data;
+                return SizedBox(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          'Select reason for your leave request: ',
+                          style: TextStyle(
+                            fontSize: 22,
+                            fontWeight: FontWeight.bold,
                           ),
-                        child: DropdownButton(
-                              hint: Text("Select Options"),
-                              dropdownColor: const Color(0xFF6392B0),
-                              icon: Icon(Icons.arrow_drop_down),
-                              iconSize: 36,
-                              isExpanded: true,
-                              underline: SizedBox(),
-                              style: TextStyle(
-                                color: Color.fromRGBO(247, 227, 112, 1.0),
-                                fontSize: 22,
-                              ),
-                              value: valueChoose,
-                              onChanged: (newValue) {
-                                setState(() {
-                                  valueChoose = newValue;
-                                });
-                              },
-                              items: listItem.map((valueItem) {
-                                return DropdownMenuItem(
-                                  value: valueItem,
-                                  child: Text(valueItem),
-                                );
-                              },
-                            ).toList(),
-                            ),
-                             
-
-
                         ),
                       ),
-      ),
-      Container(
-        padding: EdgeInsets.all(32.0),
-        child: Column(
-            children: <Widget>[
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Refrence Id",
-                ),
-                controller: refController,),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Certificate Id",
-                ),
-                controller: certController,
-              ),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "Date",
-                ),
-                controller: dateController,
-              ),
-              
-              ElevatedButton(onPressed: () async{
-                final String refid = refController.text;
-              final String certid = certController.text;
-              final String date = dateController.text;
-
-              await api.postServices(
-                refid: refid, 
-                certid: certid, 
-                date: date, 
-                verify: '0', 
-                companyId: widget.serviceModel.companyId,
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
+                        child: ListView.builder(
+                          itemCount: leaveRequests.length,
+                          itemBuilder: (context, index) {
+                            return _radioListTile(
+                              title: leaveRequests[index].title,
+                              subtitle: leaveRequests[index].subtitle,
+                              value: index + 1,
+                              details: leaveRequests[index].details,
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
                 );
-              
-              
-
-              },
-              child: Text("Submit", style: TextStyle(color: Color.fromRGBO(247, 227, 112, 1.0)),),
-              style: ElevatedButton.styleFrom(
-                primary: Color(0xFF6392B0),
-              ),
-              ),
-            ],
-            ),
+              }
+            },
+          ),
         ),
-        
-            
-             ],
-           ),
-         ),
-    
+      ),
+      
     );
   }
 }
